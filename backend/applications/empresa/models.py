c080286dc67928a2d0539e875f461f128b673260
Cristian Sáez Mardones <cristiansaez97@gmail.com>
2023-03-15 16:42:20 -0300

    Se crea modelo de empresa

--- backend/applications/empresa/models.py
+++ backend/applications/empresa/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Empresa(models.Model):
+    rut = models.CharField(verbose_name='Rut de la empresa', max_length=15, unique=True)
+    nombre = models.CharField(verbose_name='Nombre de la empresa', max_length=30)
+    direccion = models.CharField(verbose_name='Dirección de la empresa', max_length=50)
+    telefono = models.CharField(verbose_name='Télefono de la empresa', max_length=15)
+
+    def __str__(self):
+        return self.nombre
