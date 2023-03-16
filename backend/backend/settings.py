33c220c48333dbd6680c70f992800eb5e387042b
Cristian Sáez Mardones <cristiansaez97@gmail.com>
2023-03-16 02:55:54 -0300

    Se configuran los cors para conectar con angular

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,6 +36,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
     'applications.empleado',
     'applications.empresa',
     'rest_framework'
@@ -50,7 +50,11 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
 ]
+
+CORS_ALLOWED_ORIGINS = ['http://localhost:4200']
 
 ROOT_URLCONF = "backend.urls"
 
